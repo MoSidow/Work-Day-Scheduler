ac546f97d888a96dce2fd90365f297421f89b135
Mohamed Sidow <sidowmohamed35@gmail.com>
2022-10-30 18:31:46 +0000

    added click function to save button, when clicked it is saved to local storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,19 @@
+// Displays Current Day on top of Calender
 var DateNow = moment().format("dddd MMM Do, YYYY");
 $("#currentDay").text(DateNow);
+
+var saveBtn = $(".saveBtn");
+
+
+// when button is clicked, description and what hour it belongs to is saved to local storage.
+saveBtn.on("click", function() {
+
+    var Time = $(this).siblings(".hour").text();
+
+    var description = $(this).siblings(".description").val();
+
+    localStorage.setItem(Time, description);
+
+});
+
+
